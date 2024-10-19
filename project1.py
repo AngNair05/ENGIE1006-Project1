0368c7126429053601da54ff70509ecabc597463
Angelina Nair <a1nair3456@gmail.com>
2024-10-19 17:27:47 -0400

    p1a

--- project1.py
+++ project1.py
@@ -9,7 +9,35 @@ Created on Mon Oct 14 16:40:33 2024
 import csv
 
 file = open('air_quality.csv', 'r')
+dic = {}
+dic2 ={}
 
-for i in range(10): # print the first 10 lines 
-    print(file.readline().strip())
+for i in file: 
+    values = i.strip().split(",")
+    
+    if values:
+        key = values[0] 
+        key1 = values[2]
+        
+        tuple_values = tuple(values[3:])  
+        tuple_values1 = tuple(values[3:])
+        
+        if key in dic:
+            dic[key].append(tuple_values)
+            
+        else:
+            dic[key] = [tuple_values] 
+        
+        #for second dictionary
+        if key1 in dic2:
+            dic2[key1].append(tuple_values1)
+        else:
+            dic2[key1] = [tuple_values1]  
+  
+    
+#print(dic)
+print(dic2)
+
+
+
 file.close()
